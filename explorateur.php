7c3c14881042f1e286f8eec7681785e31c90d591
n-peugnet <n.peugnet@free.fr>
2017-04-03 17:41:07 +0200

    remove other specificities

--- explorateur.php
+++ explorateur.php
@@ -1,18 +1,12 @@
-<?php
-session_start();
-if (!isset($_SESSION['loggedIn']) || !$_SESSION['loggedIn'])
-	header('location: /intranet/login.php?redirect=explorateur.php');
-?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>File Explorer</title>	
-	<?php include('../head.php'); ?>
+	<title>File Explorer</title>
 	<link rel="stylesheet" href="fileExplorer.css" />
 </head>
 <body>
-	<?php include('../background.php'); ?>	
-    <h1><a href="/">CLUB1</a>›<a href="/intranet/">Intranet</a>›Explorateur</h1>
+    <h1>Explorateur</h1>
+	
 	<?php
 	require 'dossier.php';
 
